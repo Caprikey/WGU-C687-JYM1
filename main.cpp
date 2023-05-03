6e4c099a8172f6f61de1b49f7db5463d9c5f03f2
Caprikey <DantyDCook@gmail.com>
2023-05-03 16:11:01 -0400

    Revision 9, v.1.0
    Console Resizing Code.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+#include <iostream>
+#include <string>
+
+#include <iomanip>
+#include <cstring>
+
+#include <sstream>
 
 #include "header\stdafx.h"
 
@@ -6,13 +13,23 @@
 #include "header\roster.h"
 
 
-#include <iostream>
-#include <string>
 
-#include <iomanip>
-#include <cstring>
+// HEADER AND DEFINITION For Resizing A Console Window -- START
 
-#include <sstream>
+// NOTE Reference Source for following header and define information, as well as the following code in main() for resizing the console window from the following web site: https://cplusplus.com/forum/beginner/1481/
+//the following line is necessary for the
+//  GetConsoleWindow() function to work!
+//it basically says that you are running this
+//  program on Windows 2000 or higher
+#define _WIN32_WINNT 0x0500
+
+//it is important that the above line be typed
+//  BEFORE <windows.h> is included
+#include <windows.h>
+
+// HEADER AND DEFINITION For Resizing A Console Window -- END
+
+
 
 using namespace std;
 
@@ -51,6 +68,38 @@ const string studentData[] = {
 
 
 int main() {
+
+    // CONSOLE WINDOW RESIZE AND MOVING CODE - START
+    
+    // NOTE Reference Source for the above and the following code for resizing the console window from the following web site: https://cplusplus.com/forum/beginner/1481/
+    
+    HWND console = GetConsoleWindow();
+    RECT r;
+    GetWindowRect(console, &r); //stores the console's current dimensions
+
+    // MoveWindow(window_handle, x, y, width, height, redraw_window);
+    // MoveWindow(console, r.left, r.top, 1300, 800, TRUE);
+    MoveWindow(console, 350, 130, 1200, 800, TRUE);
+
+    // Prints A-Z, 100 Times -- Original Code To From Reference Used To Populate Some Data Into The Console Window
+    //for (int j = 0; j < 100; ++j)
+    // Changed the original Code to 6 instead of 100 for use attempting to size my console window to the preferred width. 
+    /*
+    for (int j = 0; j < 6; ++j)
+    {
+        for (int i = 0x41; i < 0x5B; ++i)
+            cout << (char)i;
+    }
+    cout << endl;
+    */
+
+    // Sleep and Second MoveWindow Commands below were remove to keep the cnosole from shifting to back to the original position and size after 1 second. 
+    // Original Size and Positiong of the Console window is obtained in the third line of this code block, GetWindowRect(console, &r); 
+    //Sleep(1000);
+    //MoveWindow(console, r.left, r.top, r.right - r.left, r.bottom - r.top, TRUE);
+
+    // CONSOLE WINDOW RESIZE AND MOVING CODE - START
+    // 
     //TODO: FULL CLEANUP NEEDED 
 
 
@@ -348,8 +397,6 @@ int main() {
     cout << endl;
     // ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ==== ====
     cout << endl;
-
-
 
 
     classRoster.printInvalidEmails();
